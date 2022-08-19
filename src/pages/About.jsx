b6cebe4c3483e51322d7f519e95f4479a7a51562
RogerioBrasileiro <105891672+RogerioBrasileiro@users.noreply.github.com>
2022-08-18 21:05:34 -0300

    hfh

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function About
+
+    () {
+    return (
+        <div>
+
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/user'>User</Link>
+            </nav>
+        </div>
+    )
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useNavigate} from "react-router-dom"
+
+export default function NotFound
+    () {
+    let navigate = useNavigate();
+
+    const voltar = () => {
+        navigate('/')
+    }
+    return (
+        <div>
+            <h1>Página não encontrada</h1>
+
+            <button onClick={voltar}>Voltar</button>
+
+        </div>
+    )
+}
