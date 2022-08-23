2069cc799fc6e82e2f8403da2ec384c82c3b2e31
RogerioBrasileiro <105891672+RogerioBrasileiro@users.noreply.github.com>
2022-08-23 20:08:24 -0300

    new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import User from './pages/User';
 import NotFound from './pages/NotFound';
+import UserView from './pages/UserView';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/user' element={<User />} />
+        <Route path='/user/:id' element={<UserView />} />
         <Route path='*' element={<NotFound />} />
 
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
     return (
         <div>
-            <h1>Bem vindo ao SVL</h1>
+            <h1>Bem vindo ao melhor site de filmes do Brasil</h1>
 
             <nav>
                 <Link to='/about'>About</Link>
